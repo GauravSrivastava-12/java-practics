982f8ad26f32695d51e471269410dcb9f156d898
GauravSrivastava-12 <gauravsrivastava1226@gmail.com>
2026-08-17 18:24:03 +0000

    pattern printing of squre (* )

---
+++ questions/squrepattern.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class squrepattern{
+    public static void main(String[]args){
+         
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter number of row:");
+        int r=sc.nextInt();
+        System.out.print("Enter number of column:");
+        int c=sc.nextInt();
+
+        for(int i=1;i<=r;i++){
+            for(int j=1;j<=c;j++){
+                System.out.print("* ");
+            }
+             System.out.println();
+        }
+
+    }
+}
