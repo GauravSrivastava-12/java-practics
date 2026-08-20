ebfc72615575263239247cdc545125db19ea402c
GauravSrivastava-12 <gauravsrivastava1226@gmail.com>
2026-08-20 18:18:09 +0000

    traingle pattern

---
+++ questions/traingle.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class trainglepattern{
+    public static void main(String[]args){
+         
+        Scanner sc=new Scanner(System.in);
+        // System.out.print("Enter number of row:");
+        //  int r=sc.nextInt();
+        System.out.print("Enter number of column:");
+        int c=sc.nextInt();
+
+        for(int j=1;j<=i;j++){
+            for(int i=1;i<=c;i++){
+                System.out.print("* ");
+            }
+             System.out.println();
+        }
+
+    }}
